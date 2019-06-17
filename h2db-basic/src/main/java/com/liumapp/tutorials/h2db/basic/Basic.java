80ab39270f11a20bc41cf827c212e55fb6471867
liumapp <liumapp.com@gmail.com>
2019-06-17 14:46:42 +0800

    add basic main class

---
+++ h2db-basic/src/main/java/com/liumapp/tutorials/h2db/basic/Basic.java
@@ -0,0 +1,17 @@
+package com.liumapp.tutorials.h2db.basic;
+
+/**
+ * file Basic.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/6/17
+ */
+public class Basic
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
