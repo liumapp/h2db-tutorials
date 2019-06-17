916a522c4686fa1b3be47e2b7f0a8e75545b84aa
liumapp <liumapp.com@gmail.com>
2019-06-17 15:56:54 +0800

    add h2db spring module

---
+++ h2db-spring/src/main/java/com/liumapp/tutorials/h2db/spring/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.tutorials.h2db.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
