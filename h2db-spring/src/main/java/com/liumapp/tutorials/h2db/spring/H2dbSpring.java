5c3f50c49b9dae7296862086221e3c87bcc6cc8f
liumapp <liumapp.com@gmail.com>
2019-06-17 17:32:20 +0800

    add main class for spring boot

---
+++ h2db-spring/src/main/java/com/liumapp/tutorials/h2db/spring/H2dbSpring.java
@@ -0,0 +1,21 @@
+package com.liumapp.tutorials.h2db.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file H2dbSpring.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/6/17
+ */
+@SpringBootApplication
+public class H2dbSpring {
+
+    public static void main (String[] args) {
+        SpringApplication.run(H2dbSpring.class, args);
+    }
+
+}
---
+++ h2db-spring/src/test/java/com/liumapp/tutorials/h2db/spring/package-info.java
@@ -0,0 +1,1 @@
+package com.liumapp.tutorials.h2db.spring;
