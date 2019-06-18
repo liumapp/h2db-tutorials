1fc34c3124c847481691c63b7f3e3d808f4b9024
liumapp <liumapp.com@gmail.com>
2019-06-18 14:48:48 +0800

    :heavy_plus_sign: add generator dependency

---
+++ h2db-spring-ibatis/src/main/java/com/liumapp/tutorials/h2db/ibatis/MapperGenerator.java
@@ -0,0 +1,17 @@
+package com.liumapp.tutorials.h2db.ibatis;
+
+/**
+ * file MapperGenerator.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/6/18
+ */
+public class MapperGenerator {
+
+    public static void main (String[] args) {
+
+    }
+
+}
