7547dcd699ab86b83dce24e8ca9a346f2d69e55a
liumapp <liumapp.com@gmail.com>
2019-06-18 14:47:01 +0800

    rename main class name

---
+++ h2db-spring-ibatis/src/main/java/com/liumapp/tutorials/h2db/ibatis/H2dbSpringIbatis.java
@@ -0,0 +1,21 @@
+package com.liumapp.tutorials.h2db.ibatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file H2dbSpringIbatis.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/6/18
+ */
+@SpringBootApplication
+public class H2dbSpringIbatis
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(H2dbSpringIbatis.class, args);
+    }
+}
---
+++ h2db-spring-ibatis/src/test/java/com/liumapp/tutorials/h2db/ibatis/H2dbSpringIbatisTest.java
@@ -0,0 +1,7 @@
+package com.liumapp.tutorials.h2db.ibatis;
+
+import static org.junit.Assert.*;
+
+public class H2dbSpringIbatisTest {
+
+}
